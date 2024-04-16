78dca6b6865d3d876b3ecd4ea5d34dce6074ca44
Rafael Souza <rafa.nds13@gmail.com>
2024-04-16 12:12:56 -0300

    Inserindo dados fakes de clientes

---
+++ src/main/resources/import.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Rebeca Castro Sousa', '96193454870', 3500.00, '1990-07-20', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Raissa Martins Santos', '70083845640', 7000.00, '1974-04-04', 3);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Júlia Cavalcanti Alves', '67784369212', 4000.00, '2002-07-12', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Ryan Costa Araujo', '93806443530', 5000.00, '1996-05-04', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Eduardo Rodrigues Ribeiro', '26049834350', 4000.00, '2002-10-30', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Tomás Castro Pereira', '35741501729', 6500.00, '1947-03-18', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Sarah Martins Dias', '57862239814', 10000.00, '1974-01-17', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Alice Santos Almeida', '91250872405', 3750.00, '1973-08-26', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Eduardo Pereira Azevedo', '10485522845', 5750.00, '1966-09-24', 5);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Cauã Souza Lima', '277.663.596-69', 4500.00, '1970-02-20', 0);
+
